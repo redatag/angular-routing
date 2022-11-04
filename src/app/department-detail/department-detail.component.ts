d333edac45834ba7a11e302e77ef8495ee887385
Reda Tag <rtag@ncgr.gov.sa>
2022-11-05 01:30:26 +0300

    use paramMap Observable

--- src/app/department-detail/department-detail.component.ts
+++ src/app/department-detail/department-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -7,13 +7,34 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./department-detail.component.css']
 })
 export class DepartmentDetailComponent implements OnInit {
-   departmentId: string ='';
+   departmentId!: number;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router:Router) { }
 
   ngOnInit(): void {
-    // let id = this.route.snapshot.paramMap.get('id');
-    this.departmentId = this.route.snapshot.paramMap.get('id')!;
+    // this.departmentId = parseInt(this.route.snapshot.paramMap.get('id')!);
+this.route.paramMap.subscribe(
+      (params: ParamMap) => {
+        this.departmentId  = parseInt(params.get('id')!);
+      }
+    )
   }
+
+  goPrevious(){
+    let previousId = this.departmentId - 1;
+    this.router.navigate(['/departments', previousId]);
+
+  }
+  goNext(){
+    let nextId = this.departmentId  + 1;
+    this.router.navigate(['/departments', nextId]);
+  }
+
+  //drow back of snap shot approch
+  //navigate from one comp back to same comp not work
+  //لانى لما ترجع للكومبونن السابق دالة ngOnInit مش بتتنادى 
+  //بيحصلها كول بس فى حالة تغير الinput لو مانت بتاخد input 
+  // الكود الى جود الفانكشن مش هيتنف1 
+  // لحل المشكله بنستخدم paramMap observable 
 
 }
